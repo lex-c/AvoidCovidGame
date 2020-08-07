3909c2a49a736cb476ca48b4d89092c8ebba8221
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-08-07 16:33:25 -0400

    add 3page display and add js and css according to determination

--- js/main.js
+++ js/main.js
@@ -1,24 +1,18 @@
-/* --------------
-Pseudocode: 
+const body = document.querySelector('body')
+const hmPg = document.getElementById('homePg')
+const outPg = document.getElementById('outPg')
+const spacePg = document.getElementById('spacePg')
+const hmOutBtn = document.getElementById('hmOutBtn')
+const tempOSBtn = document.getElementById('outSBtnTmp')
+const tempOHmBtn = document.getElementById('outHBtnTmp')
+const spcOutBtn = document.getElementById('spcOutBtn')
 
+hmOutBtn.addEventListener('click', chngPage)
+tempOSBtn.addEventListener('click', chngPage)
+tempOHmBtn.addEventListener('click', chngPage)
+spcOutBtn.addEventListener('click', chngPage)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
------------------- */
-document.body.style.setProperty('color', 'whitesmoke')
+function chngPage(e) {
+    document.getElementById(e.target.classList[0]).style.setProperty('display', 'none')
+    document.getElementById(e.target.classList[1]).style.setProperty('display', 'grid')
+}
