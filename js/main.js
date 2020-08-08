0d904f93d5a22aec56f719eb43a6405ed596782b
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-08-08 11:01:15 -0400

    add init displays for basics

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ function chngPage(e) {
     document.getElementById(e.target.classList[1]).style.setProperty('display', 'grid')
 }
 
+
 //HOME PAGE
 const allDivs = document.querySelectorAll('.hm-pg-div')
 const healthEl = document.getElementById('health')
@@ -29,4 +30,30 @@ const protItemsEl = document.getElementById('protItems')
 const medsEl = document.getElementById('meds')
 const hmOutDiv = document.getElementById('hmOutDiv')
 
+function hmPgInit() {
+    hmPgDispInit()
+}
 
+function hmPgDispInit() {
+    healthEl.innerHTML = `Health: ${100}`
+    mHlthEl.innerHTML = `Mental Health: ${100}`
+    foodEl.innerHTML = `Food: ${10}`
+    moneyEl.innerHTML = `Cash: $${500}`
+    protItemsEl.innerHTML = `notsure`
+    medsEl.innerHTML = `maybe`
+}
+
+
+
+
+
+
+
+
+
+
+function init() {
+    hmPgInit()
+}
+
+init()
